58745a4804a27dad2d2f92983edbc78e6416c38d
swan-of-bodom <0xHyoga@cygnusdao.finance>
2024-08-23 04:17:23 +0200

    Change fzf shortcut

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,9 +5,9 @@ return {
   event = 'VeryLazy',
   opts = { files = { previewer = 'max-perf' } },
   keys = {
-    { '<leader>f', ':FzfLua files<cr>', desc = 'Find files' },
+    { '<C-f>', ':FzfLua files<cr>', desc = 'Find files' },
     { '<C-b>', ':FzfLua buffers<cr>', desc = 'Find buffers' },
-    { '<leader>g', ':FzfLua grep<cr>', desc = 'Grep' },
+    { '<C-g>', ':FzfLua grep<cr>', desc = 'Grep' },
   },
   config = function()
     -- calling `setup` is optional for customization
